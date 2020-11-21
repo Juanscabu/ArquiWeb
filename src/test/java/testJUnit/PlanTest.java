829d1124735da0b09a4baec3b759d9009246ff28
Matias Villegas <mati.villegas23@gmail.com>
2020-11-21 17:24:03 -0300

    agregado basico de test

---
+++ src/test/java/testJUnit/PlanTest.java
@@ -0,0 +1,14 @@
+package testJUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlanTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/testJUnit/VueloTest.java
@@ -0,0 +1,14 @@
+package testJUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VueloTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
